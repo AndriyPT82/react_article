8b7bc0fcb769690a49932c22cfc3148e07f02706
Andriy Ivanyk <andriy@Air-Andriy.lan>
2021-04-27 21:55:12 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.cc003871961544b16206e1298ea5e9c1.js"
 );
 
 self.addEventListener('message', (event) => {
